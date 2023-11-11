58deb42d87baadd6829c11262243e99b7ef5abf3
Alex Kollmeyer <akollmeyer123@gmail.com>
2023-11-10 21:47:27 -0600

    Modified the UserForDisplayDto and the authentication controller register request to include the user's role.

--- FullStackAuth_WebAPI/Controllers/AuthenticationController.cs
+++ FullStackAuth_WebAPI/Controllers/AuthenticationController.cs
@@ -56,6 +56,7 @@ namespace FullStackAuth_WebAPI.Controllers
                 UserName = user.UserName,
                 FirstName = user.FirstName,
                 LastName = user.LastName,
+                Role=role
             };
             return StatusCode(201, createdUser);
         }
@@ -77,7 +78,7 @@ namespace FullStackAuth_WebAPI.Controllers
                 var userRoles = await _userManager.GetRolesAsync(loggedInUser);
 
                 // Default role
-                var role = "USER";
+                var role = "User";
 
                 // Check if the user has the "Admin" role
                 if (userRoles.Contains("Admin"))
--- FullStackAuth_WebAPI/DataTransferObjects/UserForDisplayDto.cs
+++ FullStackAuth_WebAPI/DataTransferObjects/UserForDisplayDto.cs
@@ -9,5 +9,6 @@ namespace FullStackAuth_WebAPI.DataTransferObjects
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string UserName { get; set; }
+        public string Role { get; set; }
     }
 }
